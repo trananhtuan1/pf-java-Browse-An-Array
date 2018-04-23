74b670323c7159e1d56aa6473ee0ae81f82a53e9
tuan <tincanxh@gmail.com>
2018-04-23 15:35:16 +0700

    tao phương thức in mảng

--- BrowseAnArray.java
+++ BrowseAnArray.java
@@ -4,30 +4,30 @@ public class BrowseAnArray {
     public static void main(String[] args) {
         int[] array = createRandomArray(5);
         int[] copiedArray = copyArray(array);
-        for (int x : array) {
-            System.out.print(x + "\t");
-        }
-        System.out.println(" ");
-        for(int x : copiedArray){
-            System.out.print(x + "\t");
-        }
-
+        printArray(array);
+        printArray(copiedArray);
     }
 
     public static int[] createRandomArray(int legth) {
         int[] numbers = new int[legth];
-        for(int i=0; i< numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = (int) Math.round(Math.random() * 10);
         }
         return numbers;
     }
 
-    public static int[] copyArray(int[] array){
+    public static int[] copyArray(int[] array) {
         int[] copyArray = new int[array.length];
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             copyArray[i] = array[i];
         }
         return copyArray;
     }
 
+    public static void printArray(int array[]){
+        for (int x : array) {
+            System.out.print(x + "\t");
+        }
+        System.out.println(" ");
+    }
 }
